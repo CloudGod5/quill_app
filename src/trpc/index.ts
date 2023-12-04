452c998d8b0521e09d77a624fc12fc3420d1fee2
Justin Tingley <justin@bocbanking.com>
2023-12-04 12:56:08 -0600

    authCallback

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -1,19 +1,38 @@
-import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/dist/types/server';
-import { publicProcedure, router } from './trpc';
-import { TRPCError } from '@trpc/server';
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
+import {
+  //privateProcedure,
+  publicProcedure,
+  router,
+} from './trpc'
+import { TRPCError } from '@trpc/server'
+import { db } from '@/db';
  
 export const appRouter = router({
-  authCallback: publicProcedure.query(() => {
+  authCallback: publicProcedure.query(async () => {
     const { getUser } = getKindeServerSession();
-    const user = getUser();
+    const user = await getUser();
 
-    if(!user)
-    throw new TRPCError({code: 'UNAUTHORIZED', message: 'Unauthorized'});
+    if (!user?.id || !user?.email )
+      throw new TRPCError({ code: 'UNAUTHORIZED' })
 
-    // check if the user is in the  DB
+    const dbUser = await db.user.findFirst({
+      where: {
+        id: user.id
+      }
+    });
+
+    if(!dbUser)
+    {
+      await db.user.create({
+        data: {
+          id: user.id,
+          email: user.email
+        }
+      });
+    }
+
     return { success: true };
   })
 });
- 
 
 export type AppRouter = typeof appRouter;
