e39cbc80ed0e74e12cade9ba46a41ffa27b3be7a
Justin Tingley <justin@bocbanking.com>
2023-12-04 16:32:06 -0600

    Delete File 3:24

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,14 +1,23 @@
+import Dashboard from "@/components/Dashboard";
+import { db } from "@/db";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { redirect } from "next/navigation";
 
-const Page = () => {
-    const {getUser} = getKindeServerSession()
-    const user = getUser()
+const Page = async () => {
+  const {getUser} = getKindeServerSession()
+  const user = await getUser()
 
-    if (!user) redirect('/auth-callback?origin=/dashboard')
+  if (!user || !user.id) redirect('/auth-callback?origin=/dashboard')
 
+  const dbUser = db.user.findFirst({
+    where: {
+      id: user.id
+    }
+  })
 
-    return ("Hello World!");
+  if(!dbUser) redirect('/auth-callback?origin=/dashboard')   
+
+  return <Dashboard/>
 }
 
 export default Page;
--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -1,12 +1,9 @@
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
-import {
-  //privateProcedure,
-  publicProcedure,
-  router,
-} from './trpc'
+import { privateProcedure, publicProcedure, router,} from './trpc'
 import { TRPCError } from '@trpc/server'
 import { db } from '@/db';
- 
+import { z } from 'zod';
+
 export const appRouter = router({
   authCallback: publicProcedure.query(async () => {
     const { getUser } = getKindeServerSession();
@@ -26,13 +23,48 @@ export const appRouter = router({
       await db.user.create({
         data: {
           id: user.id,
+          //firstName: user.firstName,
+          //lastName: user.lastName,
           email: user.email
         }
       });
     }
 
     return { success: true };
-  })
+  }),
+  getUserFiles: privateProcedure.query(async ({ ctx }) => {
+    const { userId, user } = ctx;
+
+    return await db.file.findMany({
+      where: {
+        userId
+      }
+    });
+}),
+deleteFile: privateProcedure.input(
+  z.object({ id: z.string() })
+).mutation(async ({ ctx, input }) => {
+  const { userId } = ctx;
+
+  const file = await db.file.findFirst({
+    where: {
+      id: input.id,
+      userId
+    }
+  });
+
+  if(!file)
+    throw new TRPCError({ code: 'NOT_FOUND' });
+
+  await db.file.delete({
+    where: {
+      id: input.id
+    }
+  });
+
+  return file;
+
+})
 });
 
 export type AppRouter = typeof appRouter;
